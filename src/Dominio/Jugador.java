8717c8a1f4220a9aaa8e90d5f394a44c45edc2bd
Kari <camargo.kari@gmail.com>
2017-04-23 15:05:41 -0300

    create user and player class

---
+++ src/Dominio/Jugador.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Dominio;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Owner
+ */
+public class Jugador extends Usuario{
+
+    private float saldo;
+    private boolean logueado;
+    private Juego juegoActivo;
+
+    //CONSTRUCTORES
+    public Jugador(String usuario, String password) {
+        super(usuario, password);
+    }
+
+    public Jugador() {
+        
+    }
+    
+     public Jugador(String usuario,String password, String nombreCompleto, float saldo) {
+        super(usuario, password,nombreCompleto);
+        this.saldo = saldo;
+    }
+
+    public float getSaldo() {
+        return saldo;
+    }
+
+    public boolean isLogueado() {
+        return logueado;
+    }
+
+    public void setLogueado(boolean logueado) {
+        this.logueado = logueado;
+    }
+
+    public Juego getJuegoActivo() {
+        return juegoActivo;
+    }
+
+    public void setJuegoActivo(Juego juegoActivo) {
+        this.juegoActivo = juegoActivo;
+    }
+
+    
+    public void agregarSaldo(float saldo) {
+        this.saldo += saldo;
+    }
+
+    public void setSaldo(float saldo) {
+        this.saldo = saldo;
+    }
+
+    boolean equalsUser(String user) {
+        return this.getNombreUsuario().equals(user);
+    }
+
+}
---
+++ src/Dominio/Usuario.java
@@ -0,0 +1,72 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Dominio;
+
+/**
+ *
+ * @author Owner
+ */
+public class Usuario {
+    
+    private String nombreCompleto;
+    private String nombreUsuario;
+    private String password;
+    
+    private int oid;
+    
+    public Usuario(String nombreUsuario, String password) {
+        this.nombreUsuario = nombreUsuario;
+        this.password = password;
+    }
+    
+    public Usuario(String nombreUsuario, String password, String nombreCompleto) {
+        this.nombreUsuario = nombreUsuario;
+        this.password = password;
+        this.nombreCompleto = nombreCompleto;
+    }
+
+    public Usuario() {
+    }
+
+    public String getNombreCompleto() {
+        return nombreCompleto;
+    }
+
+    public int getOid() {
+        return oid;
+    }
+
+    public void setOid(int oid) {
+        this.oid = oid;
+    }
+    
+    public void setNombreCompleto(String nombreCompleto) {
+        this.nombreCompleto = nombreCompleto;
+    }
+
+    public String getNombreUsuario() {
+        return nombreUsuario;
+    }
+
+    public void setNombreUsuario(String nombreUsuario) {
+        this.nombreUsuario = nombreUsuario;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        Usuario u = (Usuario)obj;
+        return u.nombreUsuario.equals(nombreUsuario) && u.password.equals(password);
+    }
+    
+}
